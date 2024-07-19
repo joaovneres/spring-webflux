08becd2bb7688d070544dbae6acba5134da1bb81
Neres <joaovneres@outlook.com>
2024-07-19 20:13:45 -0300

    feat: implements trim string validator

--- src/main/java/br/com/joaovneres/springwebflux/model/request/UserRequest.java
+++ src/main/java/br/com/joaovneres/springwebflux/model/request/UserRequest.java
@@ -1,5 +1,6 @@
 package br.com.joaovneres.springwebflux.model.request;
 
+import br.com.joaovneres.springwebflux.validator.TrimString;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -8,13 +9,16 @@ public record UserRequest(
 
         @Size(min = 3, max = 50, message = "must be between 3 and 50 characters")
         @NotBlank(message = "must not be null or  empty")
+        @TrimString
         String name,
 
         @Email(message = "invalid email")
         @NotBlank(message = "must not be null or empty")
+        @TrimString
         String email,
 
         @Size(min = 6, max = 20, message = "must be between 6 and 20 characters")
         @NotBlank(message = "must not be null or  empty")
+        @TrimString
         String password) {
 }
