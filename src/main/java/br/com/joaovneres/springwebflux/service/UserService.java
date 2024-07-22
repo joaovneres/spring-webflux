ee76ad4a67448ab043db0f755bb3e53219e5ea4c
Neres <joaovneres@outlook.com>
2024-07-22 10:43:17 -0300

    feat: implements findAll

--- src/main/java/br/com/joaovneres/springwebflux/service/UserService.java
+++ src/main/java/br/com/joaovneres/springwebflux/service/UserService.java
@@ -2,10 +2,13 @@ package br.com.joaovneres.springwebflux.service;
 
 import br.com.joaovneres.springwebflux.entity.User;
 import br.com.joaovneres.springwebflux.model.request.UserRequest;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface UserService {
     Mono<User> save(UserRequest request);
 
     Mono<User> findById(String id);
+
+    Flux<User> findAll();
 }
--- src/main/java/br/com/joaovneres/springwebflux/service/impl/UserServiceImpl.java
+++ src/main/java/br/com/joaovneres/springwebflux/service/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import br.com.joaovneres.springwebflux.service.UserService;
 import br.com.joaovneres.springwebflux.service.exception.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static java.lang.String.format;
@@ -30,5 +31,10 @@ public class UserServiceImpl implements UserService {
                 .switchIfEmpty(Mono.error(
                         new ObjectNotFoundException(
                                 format("Object not found. Id: %s, type: %s", id, User.class.getSimpleName()))));
+    }
+
+    @Override
+    public Flux<User> findAll() {
+        return userRepository.findAll();
     }
 }
